c9d940215ed955577ef36584999eb112587fe9b3
Dang Thanh Hao <aszqsc@gmail.com>
2020-10-23 23:52:12 +0700

    use context api

---
+++ chap3-learning/src/bike.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bike=()=>{
+    const a=1+2;
+
+return(
+<h1>Xe đạp {a}</h1> 
+) 
+}
+
+export default Bike;
---
+++ note-book/src/context/NoteContext.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+const noteContext=React.createContext(null)
+export default noteContext
