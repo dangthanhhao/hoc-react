b929bf3fd8bc3d03c9882434172fc721f7eb074b
Dang Thanh Hao <aszqsc@gmail.com>
2020-11-12 19:26:46 +0700

    Fix search

--- note-book/src/components/sidebar/sidebar.js
+++ note-book/src/components/sidebar/sidebar.js
@@ -4,18 +4,19 @@ import ListNote from './list-note/listNote'
 
 const SideBar = (props) => {
     const originNotes = props.notes
-    const [cloneNotes, setClone] = useState(originNotes)
-    if (cloneNotes!=originNotes)
-        setClone(originNotes)
+    const [cloneNotes, setClone] = useState({search:null})
+  
+    
     const search = (event) => {
-        setClone(originNotes.filter(item=>item.title.includes(event.target.value)))
+        setClone({notes: originNotes.filter(item=>item.title.includes(event.target.value)), search: event.target.value})
     }
+    const listToShow= (cloneNotes.search)? originNotes.filter(item=>item.title.includes(cloneNotes.search))  : originNotes
 
     return (
         <div className="col-sm-4">
             <SearchBar search={search}></SearchBar>
             <h5>All notes</h5>
-            <ListNote notes={cloneNotes} seletedAction={props.seletedAction} seletednotes={props.seletednotes}></ListNote>
+            <ListNote notes={listToShow} seletedAction={props.seletedAction} seletednotes={props.seletednotes}></ListNote>
         </div>
     )
 }
